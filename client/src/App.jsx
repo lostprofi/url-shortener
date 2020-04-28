ca76a1c9ac29bb216769e2ac4688bf6906a8d5f7
lostprofi <o-lazarchik@mail.ru>
2020-04-28 10:33:58 +0300

    start developing client reg

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import { Grid, Typography, AppBar } from '@material-ui/core';
+import 'typeface-roboto';
+
+function App() {
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <AppBar position="static">
+          kljl
+        </AppBar>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default App;
